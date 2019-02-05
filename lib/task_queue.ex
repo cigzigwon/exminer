fe88e6f8a747f736e24b68c694d75e85e21010b3
cigzigwon <factchkr@gmail.com>
2019-02-05 12:28:30 -0500

    Shorten comment

--- lib/task_queue.ex
+++ lib/task_queue.ex
@@ -1,6 +1,5 @@
 defmodule Miner.TaskQueue do
 	# is a queueue of tasks
-	# is a multi element tuple of maps
 	# {%{url: "", xquery: ""}, %{url: "", xquery: ""}, %{url: "", xquery: ""}}
 
 	def start_link() do
