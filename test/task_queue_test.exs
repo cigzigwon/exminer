ff6cab351a9c0ddac22650d1c0b84554f530757e
cigzigwon <factchkr@gmail.com>
2019-02-15 22:23:22 -0500

    Refactor task queue tests

--- test/task_queue_test.exs
+++ test/task_queue_test.exs
@@ -3,34 +3,34 @@ defmodule Miner.TaskQueueTest do
   doctest Miner.TaskQueue
 
   setup do
-    item = %{url: "https://www.google.com", xdq: ""}
+    item = %{url: "https://www.google.com"}
     q = start_supervised!(Miner.TaskQueue)
     Miner.TaskQueue.add(q, item)
-    %{queue: q}
+    %{queue: q, item: item}
   end
 
-  test "can add tasks to the queue", %{queue: q} do
+  test "can add tasks to the queue", %{queue: q, item: item} do
   	assert Miner.TaskQueue.get(q) == {item}
   end
 
-  test "can get a task by index", %{queue: q} do
+  test "can get a task by index", %{queue: q, item: item} do
   	assert Miner.TaskQueue.getAtIndex(q, 0) == item
   	assert Miner.TaskQueue.getAtIndex(q, 1) == nil
   end
 
-  test "can get a newly added task", %{queue: q} do
+  test "can get a newly added task", %{queue: q, item: item} do
     Miner.TaskQueue.add(q, item)
     assert Miner.TaskQueue.getAtIndex(q, 1) == item
   end
 
-  test "can update a task with response data", %{queue: q} do
+  test "can update a task with response data", %{queue: q, item: item} do
     item = Map.put(item, :resp, "<html></html>")
     Miner.TaskQueue.update(q, 0, item)
     assert Miner.TaskQueue.getAtIndex(q, 0).resp == item.resp
   end
 
-  test "can replace existing state", %{queue: q} do
-    tasks = {%{url: "https://www.google.com", xdq: ""}, %{url: "https://www.stawars.com", xdq: ""}}
+  test "can replace existing state", %{queue: q, item: item} do
+    tasks = {item, item}
     Miner.TaskQueue.replace(q, tasks)
     assert Miner.TaskQueue.get(q) == tasks
   end
