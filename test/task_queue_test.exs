2251117adf2e9f038d203f0aa7674c6c92e47df3
cigzigwon <factchkr@gmail.com>
2019-02-15 22:18:34 -0500

    Remove useless test due to immutablity

--- test/task_queue_test.exs
+++ test/task_queue_test.exs
@@ -5,32 +5,31 @@ defmodule Miner.TaskQueueTest do
   setup do
     item = %{url: "https://www.google.com", xdq: ""}
     q = start_supervised!(Miner.TaskQueue)
-    %{queue: q, item: item}
+    Miner.TaskQueue.add(q, item)
+    %{queue: q}
   end
 
-  test "can add tasks to the queue", %{queue: q, item: item} do
-  	Miner.TaskQueue.add(q, item)
+  test "can add tasks to the queue", %{queue: q} do
   	assert Miner.TaskQueue.get(q) == {item}
   end
 
-  test "can get a task by index", %{queue: q, item: item} do
-  	Miner.TaskQueue.add(q, item)
+  test "can get a task by index", %{queue: q} do
   	assert Miner.TaskQueue.getAtIndex(q, 0) == item
   	assert Miner.TaskQueue.getAtIndex(q, 1) == nil
-  	
-    Miner.TaskQueue.add(q, item)
-  	assert Miner.TaskQueue.getAtIndex(q, 1) == item
   end
 
-  test "can update a task with response data", %{queue: q, item: item} do
+  test "can get a newly added task", do
     Miner.TaskQueue.add(q, item)
+    assert Miner.TaskQueue.getAtIndex(q, 1) == item
+  end
+
+  test "can update a task with response data", %{queue: q} do
     item = Map.put(item, :resp, "<html></html>")
     Miner.TaskQueue.update(q, 0, item)
     assert Miner.TaskQueue.getAtIndex(q, 0).resp == item.resp
   end
 
-  test "can replace existing state", %{queue: q, item: item} do
-    Miner.TaskQueue.add(q, item)
+  test "can replace existing state", %{queue: q} do
     tasks = {%{url: "https://www.google.com", xdq: ""}, %{url: "https://www.stawars.com", xdq: ""}}
     Miner.TaskQueue.replace(q, tasks)
     assert Miner.TaskQueue.get(q) == tasks
