357df9d89ac7269ca7550a72ab3b9e62bbcdc3de
cigzigwon <factchkr@gmail.com>
2019-03-21 14:00:44 -0400

    Inner crawls to the TLD only

--- lib/crawler.ex
+++ lib/crawler.ex
@@ -2,13 +2,14 @@ defmodule Miner.Crawler do
 	require Logger
 
 	@valid_http ["http://", "https://"]
+	@not_allowed ["mailto:", "tel:"]
 
 	def crawl(domain) do
 		domain
 		|> fetch_body
 		|> Floki.find("a")
 		|> Floki.attribute("href")
-		|> Enum.map(fn(url) -> fetch_body(url) end)
+		|> Enum.map(fn (url) -> fetch_body(domain, url |> validate) end)
 
 		:ok
 	end
@@ -23,8 +24,33 @@ defmodule Miner.Crawler do
 		end
 	end
 
+	defp fetch_body(domain, url) do
+		url =
+		if not String.contains?(url, @valid_http) and String.length(url) > 1 do
+			domain <> url
+		else
+			url
+		end
+
+		if String.contains?(url, domain) do
+			log url
+			res = HTTPoison.get! url
+			res.body
+		else
+			""
+		end
+	end
+
 	defp log(str) do
 		Logger.info str
 		str
+	end
+
+	defp validate(href) do
+		if String.contains?(href, @not_allowed) do
+			""
+		else
+			href
+		end
 	end
 end
